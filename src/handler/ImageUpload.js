f14196a1e7c11afe5c86f2b9233fb424cf392013
phuc <quangphuc1313@gmail.com>
2023-06-20 12:34:36 +0700

    púh

--- src/handler/ImageUpload.js
+++ src/handler/ImageUpload.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const path = "https://api.cloudinary.com/v1_1/ddtagvynp/upload";
+const path = "https://api.cloudinary.com/v1_1/dwgcr1rbq/upload";
 
 const imageUpload = async (img) => {
   const data = new FormData();
