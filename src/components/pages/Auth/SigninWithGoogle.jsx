640e7da59c5396b0ea59669f4c23301c1f8ed525
Bui Huu Dat <dat54261001@gmail.com>
2023-06-21 18:53:54 +0700

    fix cart order

--- src/components/pages/Auth/SigninWithGoogle.jsx
+++ src/components/pages/Auth/SigninWithGoogle.jsx
@@ -1,8 +1,8 @@
-import { GoogleLogin } from 'react-google-login';
+import { GoogleLogin } from "react-google-login";
 
 const responseGoogle = (response) => {
   console.log(response);
-}
+};
 
 const SigninWithGoogle = () => {
   <GoogleLogin
@@ -10,8 +10,8 @@ const SigninWithGoogle = () => {
     buttonText="Login"
     onSuccess={responseGoogle}
     onFailure={responseGoogle}
-    cookiePolicy={'single_host_origin'}
-  />
-}
+    cookiePolicy={"single_host_origin"}
+  />;
+};
 
-export default SigninWithGoogle
+export default SigninWithGoogle;
